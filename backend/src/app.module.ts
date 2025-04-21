3494eb5f1aabdb30106410cbaeda6b6fb5248c57
leocodeio <basicleo787@gmail.com>
2025-04-21 11:45:55 +0530

    perf(health updated): health updated

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,44 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from '@leocodeio-njs/njs-config';
+import {
+  HealthModule,
+  PerformanceInterceptor,
+  ResponseInterceptor,
+} from '@leocodeio-njs/njs-health';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigService } from '@leocodeio-njs/njs-config';
+import { PrometheusService } from '@leocodeio-njs/njs-health/dist/health/prometheus.service';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [
+    AppConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      inject: [AppConfigService],
+      useFactory: (configService: AppConfigService) => ({
+        ...configService.databaseConfig,
+        entities: [__dirname + '/**/*.schema{.ts,.js}'],
+        migrations: [__dirname + '/database/migrations/**/*{.ts,.js}'],
+        migrationsRun: true,
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+    HealthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    PrometheusService,
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: PerformanceInterceptor,
+    },
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: ResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
